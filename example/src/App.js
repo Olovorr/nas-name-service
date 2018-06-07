c57ca121b9da8f7cb05d6e0b02172f07258a4229
Oliver Rýdzi <orydzi@gmail.com>
2018-06-07 19:07:36 +0200

    Added basic working example

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getAddressName } from 'nas-name-service'
 
 class App extends Component {
+  state = {
+    addressData: {}
+  }
+  componentDidMount() {
+    getAddressName('n1LcVhbyCp6eHAAYda4g3Z84HCTBbRCVHdm')
+      .then(({ result: { result } }) => {
+        this.setState({
+          addressData: JSON.parse(result)
+        })
+      })
+  }
   render() {
+    const {
+      addressData,
+    } = this.state
+    console.log('addressData', addressData)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to NAS Name Service </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Address Name: { addressData && addressData.name }
         </p>
       </div>
     );
--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const getAddressName = function(walletAddress) {
-   fetch("https://testnet.nebulas.io/v1/user/call", {
-      credentials: 'omit',
-      headers: {},
-      referrerPolicy: 'no-referrer-when-downgrade',
-      body: JSON.stringify(
-          from: 'n1ytsHPBkJ4Zthjhe12QTEdrKFqLZ9DrbNt',
-          to: 'n1ytsHPBkJ4Zthjhe12QTEdrKFqLZ9DrbNt',
-          value: '0',
-          nonce: 301,
-          gasPrice: 1000000,
-          gasLimit: 200000,
-          contract: {
-              function: getAddressName,
-              args: JSON.stringify(
-                  [
-                      walletAddress
-                  ]
-              )
-          },
-          method: 'POST',
-          mode: 'cors'
-      )
-  });
+    const body = { 
+        from: 'n1ytsHPBkJ4Zthjhe12QTEdrKFqLZ9DrbNt',
+        to: 'n1ytsHPBkJ4Zthjhe12QTEdrKFqLZ9DrbNt',
+        value: '0',
+        nonce: 301,
+        gasPrice: '1000000',
+        gasLimit: '200000',
+        contract: {
+            function: 'getAddressName',
+            args: JSON.stringify(
+                [
+                    walletAddress
+                ]
+            )
+        },
+    }
+    return fetch("https://testnet.nebulas.io/v1/user/call", {
+        credentials: 'omit',
+        headers: {},
+        referrerPolicy: 'no-referrer-when-downgrade',
+        body: JSON.stringify(body),
+        method: 'POST',
+        mode: 'cors'
+    }).then(function(response) {
+      return response.json()
+    });
 }
 
-module.export = {
+module.exports = {
   getAddressName: getAddressName,
 }
